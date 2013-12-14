5ff89bde95ea22449cd369a2989301c79265edd0
tyganit@gmail.com <tyganit@gmail.com@d1d990db-8151-2738-daef-f456ab3e3201>
2013-12-14 22:26:38 +0000

    New unit move.

--- trunk/Unity project/Assets/Resources/Scripts/MoveToObject.cs
+++ trunk/Unity project/Assets/Resources/Scripts/MoveToObject.cs
@@ -8,14 +8,13 @@ public class MoveToObject : MonoBehaviour {
 
 	private Quaternion _rotateTo;
 	private Vector3 _target;
-	private bool _elevate;
+	private Vector3 _relOriginForwardRay;
 	
 	public Transform Target
 	{
 		set
 		{
 			_target = value.position;
-			_target.y = transform.position.y;
 			Reset();
 		}
 	}
@@ -27,39 +26,44 @@ public class MoveToObject : MonoBehaviour {
 	}
 
 	void Update() {
-		if (transform.position == _target)
-			enabled = false;
+		Vector3 vDest = _target;
+		vDest.y = transform.position.y;
 
+		// Face the destination
 		if (transform.rotation != _rotateTo)
 			transform.rotation = Quaternion.RotateTowards(transform.rotation, _rotateTo, RotateSpeed * Time.deltaTime);
-		else if (_elevate)
+		else if (transform.position != vDest)
 		{
-			transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.up, MoveSpeed * Time.deltaTime);
-			_target.y = transform.position.y;
+			// Climb up
+			if (Physics.Raycast(transform.position + _relOriginForwardRay, transform.forward, vDest.magnitude))
+				transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.up, MoveSpeed * Time.deltaTime);
+			// Move toward target
+			else
+				transform.position = Vector3.MoveTowards(transform.position, vDest, MoveSpeed * Time.deltaTime);
 		}
+		// Climb down
+		else if (!Physics.Raycast(collider.bounds.center + Vector3.down * (collider.bounds.size.y/2), Vector3.down, 1f))
+			transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.down, MoveSpeed * Time.deltaTime);
+		// End of the road
 		else
-			transform.position = Vector3.MoveTowards(transform.position, _target, MoveSpeed * Time.deltaTime);
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		_elevate = true;
-	}
-
-	void OnTriggerExit(Collider other)
-	{
-		_elevate = false;
+			enabled = false;
 	}
 
 	public void Reset()
 	{
 		Vector3 moveDir = getMoveDirection();
 		_rotateTo = Quaternion.LookRotation(moveDir);
+
+		// Compute the ray to check for collision in front of the unit
+		_relOriginForwardRay = collider.bounds.center - transform.position;
+		_relOriginForwardRay.z = _relOriginForwardRay.z + collider.bounds.size.z/2;
+		_relOriginForwardRay.y = _relOriginForwardRay.y - collider.bounds.size.y/2 - 5;
 	}
 
 	private Vector3 getMoveDirection()
 	{
 		Vector3 direction = _target - transform.position;
+		direction.y = 0;
 		return direction.normalized;
 	}
 }
