5d156a870249f14b22150e5aad01b7b8cc973c51
flal34 <flal34@gmail.com>
2013-12-14 22:09:44 +0000

    début des sources élémentaires petits changement dans BlocFactory revert du materiel Bloc_Pierre qui a été accidentellement modifié

---
+++ Unity project/Assets/Resources/Scripts/Sources/Source.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Source : MonoBehaviour, PhaseEventListener
+{
+	public enum SourceType
+	{
+		Sand,
+		Lava,
+		Electricity,
+		Wind,
+		Water
+	}
+
+	protected SourceType _type;
+
+	protected int _generate;
+	protected int _duration;
+
+	public void Initialize(SourceInfo source)
+	{
+		_type = source.type;
+
+		_generate = source.generate;
+		_duration = source.duration;
+	}
+
+	public void onEndPhase(GameTickerEvent e)
+	{
+		RunSource();
+	}
+
+	public void onStartNewPhase(GameTickerEvent e)
+	{
+		UpdateSourceState();
+	}
+
+	protected abstract void RunSource();
+
+	protected void UpdateSourceState()
+	{
+		--_duration;
+
+		if(_duration == 0)
+		{
+			//TODO remove source from game
+		}
+	}
+}
---
+++ Unity project/Assets/Resources/Scripts/Stream.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stream : MonoBehaviour
+{
+	public enum StreamType
+	{
+		Sand,
+		Lava,
+		Water
+	}
+
+	private StreamType _type;
+
+	public void Initialize(StreamType type)
+	{
+		_type = type;
+	}
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+}
