e12378d41190e82277f76de902c910d428ea1835
tyganit <tyganit@gmail.com>
2013-12-14 11:38:10 +0000

    Une unité peut être sélectionnée et déplacée vers un bloc.

---
+++ Unity project/Assets/Resources/Scripts/SelectBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectBehaviour : MonoBehaviour {
+	public Color DisabledColor = new Color(55,55,55);
+	public Color ActiveColor = new Color(235,235,235);
+
+	private bool _isSelected;
+	public bool IsSelected
+	{
+		get { return _isSelected; }
+		set
+		{
+			_isSelected = value;
+			renderer.material.color = value ? ActiveColor : DisabledColor;
+		}
+	}
+
+	void Start()
+	{
+		IsSelected = false;
+	}
+
+	void Select()
+	{
+		IsSelected = true;
+	}
+
+	void Unselect()
+	{
+		IsSelected = false;
+	}
+}
