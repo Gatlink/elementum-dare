214fd1c7516f065d4827bb611fda9b8fdeb73c1d
flal34@gmail.com <flal34@gmail.com@d1d990db-8151-2738-daef-f456ab3e3201>
2013-12-14 23:52:47 +0000

    Reorganizing scripts

---
+++ trunk/Unity project/Assets/Resources/Scripts/Selection/SelectUnit.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectUnit : MonoBehaviour {
+	private static Collider _selected;
+	public static Collider Selected
+	{
+		get { return _selected; }
+		set
+		{
+			if (_selected != null)
+				Selected.SendMessage("Unselect");
+
+			_selected = value;
+
+			if (_selected != null)
+				Selected.SendMessage("Select");
+		}
+	}
+
+	void Update()
+	{
+		if (!Input.GetMouseButtonDown(0))
+			return;
+
+		Vector3 mousePos = Input.mousePosition;
+
+		RaycastHit hit = new RaycastHit();
+		Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+		if(Selected != null) //Something is selected already
+		{
+			string tag = Selected.tag;
+			if(tag == "Unit") //It is a UNIT
+			{
+				int layerMask = 1 << LayerMask.NameToLayer("Units");
+				//Check only against objects in the layer "Units", which contains only units
+
+				if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) //clicked on a unit
+				{
+					Selected = (hit.collider == Selected) ? null : hit.collider;
+				}
+				else //not clicked on a unit
+				{
+					layerMask = 1 << LayerMask.NameToLayer("Terrain");
+					//Check only against objects in the layer "Terrain", which contains only blocs
+					
+					if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) //clicked on a block
+					{
+						MoveToObject script = Selected.GetComponent<MoveToObject>();
+						script.Target = hit.collider.transform;
+						script.enabled = true;
+					}
+					else
+						Selected = null;
+				}
+			}
+			else if(tag == "Bloc") //It is a UNIT
+			{
+
+			}
+		}
+		else //nothing selected
+		{
+			if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+			{
+				Selected = hit.collider;
+			}
+		}
+	}
+}
