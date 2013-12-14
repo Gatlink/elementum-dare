1d513007719d41e1c1f9d3806bbe4f25e23a98e8
tyganit@gmail.com <tyganit@gmail.com@d1d990db-8151-2738-daef-f456ab3e3201>
2013-12-14 14:11:25 +0000

    Unit can now move up

--- trunk/Unity project/Assets/Resources/Scripts/MoveToObject.cs
+++ trunk/Unity project/Assets/Resources/Scripts/MoveToObject.cs
@@ -4,31 +4,52 @@ using System.Collections;
 public class MoveToObject : MonoBehaviour {
 
 	public Transform Target;
-	public float MoveStep;
-	public float RotateStep;
+	public float MoveSpeed;
+	public float RotateSpeed;
 
 	private Quaternion _rotateTo;
 	private Vector3 _target;
+	private bool _elevate;
 
 	void Start() {
 		enabled = false;
 
-		Vector3 moveDir = getMoveDirection();
-		_rotateTo = new Quaternion();
-		_rotateTo.SetFromToRotation(transform.forward, moveDir);
-
-		_target = Target.position;
-		_target.y = transform.position.y;
+		Reset();
 	}
 
 	void Update() {
-		if (transform.position == Target.position)
+		if (transform.position == _target)
 			enabled = false;
 
 		if (transform.rotation != _rotateTo)
-			transform.rotation = Quaternion.Lerp(transform.rotation, _rotateTo, RotateStep * Time.deltaTime);
+			transform.rotation = Quaternion.Lerp(transform.rotation, _rotateTo, RotateSpeed * Time.deltaTime);
+		else if (_elevate)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.up, MoveSpeed * Time.deltaTime);
+			_target.y = transform.position.y;
+		}
 		else
-			transform.position = Vector3.MoveTowards(transform.position, _target, MoveStep * Time.deltaTime);
+			transform.position = Vector3.MoveTowards(transform.position, _target, MoveSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		_elevate = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		_elevate = false;
+	}
+
+	void Reset()
+	{
+		Vector3 moveDir = getMoveDirection();
+		_rotateTo = new Quaternion();
+		_rotateTo.SetFromToRotation(transform.forward, moveDir);
+		
+		_target = Target.position;
+		_target.y = transform.position.y;
 	}
 
 	private Vector3 getMoveDirection()
