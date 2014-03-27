7623c421f80d964a87257396232df7edb23dfcf7
thibmorvan <thibmorvan@gmail.com>
2014-03-27 17:36:00 +0000

    Logo fini (a priori)

--- Unity project/Assets/Resources/Scripts/Menu/MainMenu.cs
+++ Unity project/Assets/Resources/Scripts/Menu/MainMenu.cs
@@ -20,7 +20,7 @@ public class MainMenu : MonoBehaviour {
 
 		GUI.skin = gameObject.GetComponent<CharacterSelection> ().mainMenuSkin;
 
-		GUI.Box (new Rect (Screen.width / 4,Screen.height / 8,Screen.width * 3 / 4 ,Screen.height * 3 / 8), mainLogo, logoBox);
+		GUI.Box (new Rect (Screen.width / 2 - 256, 10, 512 ,256), mainLogo, logoBox);
 
 		//lance le menu de selection des personnages.
 		if (GUI.Button (new Rect(Screen.width / 2 - 90,Screen.height / 2 + 30,180,60),"Play")){
