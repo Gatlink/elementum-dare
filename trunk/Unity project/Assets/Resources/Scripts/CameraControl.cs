a8a8575daaa0e894167a184ac7f4d63be8b0162d
tyganit@gmail.com <tyganit@gmail.com@d1d990db-8151-2738-daef-f456ab3e3201>
2014-04-13 19:07:49 +0000

    Add a zoom

--- trunk/Unity project/Assets/Resources/Scripts/CameraControl.cs
+++ trunk/Unity project/Assets/Resources/Scripts/CameraControl.cs
@@ -7,7 +7,8 @@ public class CameraControl : MonoBehaviour
     public bool QWERTY;
 #endif
 
-    public float speed = 70.0f;
+    public float Speed = 70f;
+    public float ZoomSpeed = 150f;
 
     private Transform _target;
     public Unit Target
@@ -31,8 +32,8 @@ public class CameraControl : MonoBehaviour
         {
             if (transform.position != _target.position)
             {
-                var distance = Vector3.Distance(_target.position, transform.position);
-                var interpolation = 1 - Mathf.Exp(-distance * Time.deltaTime * 0.5f - 0.1f);
+                float distance = Vector3.Distance(_target.position, transform.position);
+                float interpolation = 1 - Mathf.Exp(-distance * Time.deltaTime * 0.5f - 0.1f);
                 transform.position = Vector3.MoveTowards(transform.position, _target.position, interpolation * 10f);
             }
             else
@@ -48,16 +49,23 @@ public class CameraControl : MonoBehaviour
             if (Input.GetKey(KeyCode.E))
                 transform.Rotate(0f, -Mathf.PI / 2f, 0f);
             if ((QWERTY && Input.GetKey(KeyCode.A)) || (!QWERTY && Input.GetKey(KeyCode.Q)))
-                transform.position = transform.position + transform.right * Time.deltaTime * speed;
+                transform.position = transform.position + transform.right * Time.deltaTime * Speed;
             if (Input.GetKey(KeyCode.D))
-                transform.position = transform.position - transform.right * Time.deltaTime * speed;
+                transform.position = transform.position - transform.right * Time.deltaTime * Speed;
             if (Input.GetKey(KeyCode.S))
-                transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+                transform.position = transform.position + transform.forward * Time.deltaTime * Speed;
             if ((QWERTY && Input.GetKey(KeyCode.W)) || (!QWERTY && Input.GetKey(KeyCode.Z)))
-                transform.position = transform.position - transform.forward * Time.deltaTime * speed;
+                transform.position = transform.position - transform.forward * Time.deltaTime * Speed;
 
             if (Input.GetKeyDown(KeyCode.Tab))
                 Target = Selector.Selected;
+
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            if (scroll != 0)
+            {
+                Vector3 To = Camera.main.transform.position + Camera.main.transform.forward * ZoomSpeed * scroll;
+                iTween.MoveTo(Camera.main.gameObject, iTween.Hash("position", To, "easetype", iTween.EaseType.easeOutQuad, "time", 0.3f));
+            }
         }
 	}
 }
